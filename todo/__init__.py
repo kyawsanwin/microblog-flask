cc93027badc6540c8c1058f28657fdc696782824
Kyaw San Win <kyawsanwin@aceplussolutions.com>
2024-09-03 23:06:34 +0630

    Add db and test code.

--- todo/__init__.py
+++ todo/__init__.py
@@ -5,9 +5,15 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY=str(uuid.uuid4())
+        SECRET_KEY=str(uuid.uuid4()),
+        DATABASE=os.path.join(app.instance_path, 'todo.sqlite'),
     )
     
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+        
     try:
         os.makedirs(app.instance_path)
     except OSError:
@@ -16,5 +22,8 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World'
+    
+    from .import db
+    db.init_app(app)
     
     return app
