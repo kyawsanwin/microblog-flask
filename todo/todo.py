ccf21f47178c54ff392b6e391980424f167ed652
Kyaw San Win <zues88@gmail.com>
2024-09-07 15:30:28 +0630

    Add todo listing and add function with UI.

--- todo/todo.py
+++ todo/todo.py
@@ -1,12 +1,38 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, request, flash, g
 )
 
 from todo.auth import login_required
+from todo.db import get_db
 
-bp = Blueprint('todo', __name__)
+bp = Blueprint('todo', __name__, url_prefix='/todos')
 
-@bp.route('/todos')
+@bp.route('/', methods=('GET', 'POST'))
 @login_required
 def index():
-    return render_template('todo/index.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        
+        error = None
+        
+        if not title:
+            error = 'Title is required.'
+        
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO todos (title, user_id)'
+                ' VALUES (?, ?)',
+                (title, g.user['id'])
+            )
+            db.commit()
+
+    db = get_db()
+    todos = db.execute(
+        'SELECT t.id, title, is_done FROM todos t'
+        ' JOIN users u ON t.user_id = u.id'
+        ' ORDER BY is_done ASC'
+    ).fetchall()
+    return render_template('todo/index.html', todos=todos)  
